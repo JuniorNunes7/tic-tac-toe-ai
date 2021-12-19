ceac286f38955f323a0765c70b234f1ba675b144
Junior Nunes <nunes.junior92@gmail.com>
2021-12-19 12:15:04 -0300

    improving learn script

--- app/router.js
+++ app/router.js
@@ -4,73 +4,73 @@ const History = require('./history.model');
 
 // Verifica e retorna as melhores possibilidades.
 router.get('/check', function(req, res) {
-    const query = req.query.history;
-    const step = (query.match(/;/g) || []).length;
-    const startPlayer = query.charAt(0);
-    const winPoints = (startPlayer === 'O') ? 2 : 1;
-    const drawPoints = (startPlayer !== 'O') ? 2 : 0;
-
-    History.aggregate([
-        {
-            $match: {
-            "steps": { "$regex": "^" + query }
-            }
-        },
-        {
-            $addFields: {
-            nextStep: {
-                $arrayElemAt: [ { $split: ["$steps", ";"] }, step+1 ]
-            }
-            }
-        },
-        {
-            $group: {
-            _id: "$nextStep",
-            winRate: {
-                $sum: {
-                $switch: {
-                    branches: [
-                    { case: { $eq: ["$winner", "O"] }, then: winPoints },
-                    { case: { $eq: ["$winner", "X"] }, then: -1 },
-                    { case: { $eq: ["$winner", "D"] }, then: drawPoints }
-                    ]
-                }
-                }
-            }
-            }
+  const query = req.query.history;
+  const step = (query.match(/;/g) || []).length;
+  const startPlayer = query.charAt(0);
+  const winPoints = (startPlayer === 'O') ? 2 : 1;
+  const drawPoints = (startPlayer !== 'O') ? 2 : 0;
+  
+  History.aggregate([
+    {
+      $match: {
+        "steps": { "$regex": "^" + query }
+      }
+    },
+    {
+      $addFields: {
+        nextStep: {
+          $arrayElemAt: [ { $split: ["$steps", ";"] }, step+1 ]
         }
-    ]).exec().then((result) => {
-        let moves = {};
-        for(let i in result) {
-            let r = result[i];
-            moves[r._id.substring(1)] = r.winRate;
+      }
+    },
+    {
+      $group: {
+        _id: "$nextStep",
+        winRate: {
+          $sum: {
+            $switch: {
+              branches: [
+                { case: { $eq: ["$winner", "O"] }, then: winPoints },
+                { case: { $eq: ["$winner", "X"] }, then: -1 },
+                { case: { $eq: ["$winner", "D"] }, then: drawPoints }
+              ]
+            }
+          }
         }
-        res.end(JSON.stringify(moves));
-    }).catch((e) => {
-        res.end('[]');
-    });
-
+      }
+    }
+  ]).exec().then((result) => {
+    let moves = {};
+    for(let i in result) {
+      let r = result[i];
+      moves[r._id.substring(1)] = r.winRate;
+    }
+    res.end(JSON.stringify(moves));
+  }).catch((e) => {
+    res.end('[]');
+  });
+  
 });
 
 // Verifica e retorna as melhores possibilidades.
 router.post('/register-history', function(req, res, next) {
-    const data = {
-        winner: req.body.winner,
-        steps: req.body.steps
-    };
-    History.countDocuments(data).then((count) => {
-        if (count === 0) {
-            let history = new History(data);
-        
-            history.save(function (err) {
-                if (err) {
-                    return next(err);
-                }
-        
-                res.send(true);
-            });
+  const data = {
+    winner: req.body.winner,
+    steps: req.body.steps
+  };
+  History.countDocuments(data).then((count) => {
+    if (count === 0) {
+      let history = new History(data);
+      
+      history.save(function (err) {
+        if (err) {
+          return next(err);
         }
-    });
+        
+        res.send(true);
+      });
+    }
+  });
 });
 
 module.exports = router;
--- learn.js
+++ learn.js
@@ -9,126 +9,132 @@ const History = require('./app/history.model');
 var core = require('./core');
 
 // Define um vencedor ou empate
-function setWinner (player, gameHistory) {
-    let data = {};
-    data.steps = gameHistory.join(';');
-    data.winner = (player) ? player : 'D';
-
-    History.countDocuments(data).then((count) => {
-        if (count === 0) {
-            console.log('learning', data.steps);
-            let history = new History(data);
-            history.save();
-        }
-    });
+async function setWinner (player, gameHistory) {
+  let data = {};
+  data.steps = gameHistory.join(';');
+  data.winner = (player) ? player : 'D';
+  
+  const count = await History.countDocuments(data)
+  if (count === 0) {
+    console.log('Learning:', data.steps);
+    let history = new History(data);
+    await history.save();
+  }
 }
 
 function checkBestChoice (player, steps) {
-    if (steps.length === 0) return Promise.resolve([]);
-
-    const step = (steps.match(/;/g) || []).length;
-    const startPlayer = steps.charAt(0);
-    const winPoints = (player === startPlayer) ? 2 : 1;
-    const drawPoints = (player !== startPlayer) ? 2 : 0;
-
-    return History.aggregate([
-        {
-            $match: {
-            "steps": { "$regex": "^" + steps }
-            }
-        },
-        {
-            $addFields: {
-                nextStep: {
-                    $arrayElemAt: [ { $split: ["$steps", ";"] }, step+1 ]
-                }
-            }
-        },
-        {
-            $group: {
-            _id: "$nextStep",
-            winRate: {
-                $sum: {
-                    $switch: {
-                        branches: [
-                            { case: { $eq: ["$winner", "O"] }, then: (player === 'O' ? winPoints : -1) },
-                            { case: { $eq: ["$winner", "X"] }, then: (player === 'X' ? winPoints : -1) },
-                            { case: { $eq: ["$winner", "D"] }, then: drawPoints }
-                        ]
-                    }
-                }
-            }
-            }
+  if (steps.length === 0) return Promise.resolve([]);
+  
+  const step = (steps.match(/;/g) || []).length;
+  const startPlayer = steps.charAt(0);
+  const winPoints = (player === startPlayer) ? 2 : 1;
+  const drawPoints = (player !== startPlayer) ? 2 : 0;
+  
+  return History.aggregate([
+    {
+      $match: {
+        "steps": { "$regex": "^" + steps }
+      }
+    },
+    {
+      $addFields: {
+        nextStep: {
+          $arrayElemAt: [ { $split: ["$steps", ";"] }, step+1 ]
         }
-    ]).exec();
+      }
+    },
+    {
+      $group: {
+        _id: "$nextStep",
+        winRate: {
+          $sum: {
+            $switch: {
+              branches: [
+                { case: { $eq: ["$winner", "O"] }, then: (player === 'O' ? winPoints : -1) },
+                { case: { $eq: ["$winner", "X"] }, then: (player === 'X' ? winPoints : -1) },
+                { case: { $eq: ["$winner", "D"] }, then: drawPoints }
+              ]
+            }
+          }
+        }
+      }
+    }
+  ]).exec();
 }
 
-function doRandomGame (player, gameHistory, availablePlaces) {
+async function doRandomGame (player, gameHistory, availablePlaces) {
+  availablePlaces = availablePlaces.sort(() => Math.random() - 0.5)
+  const place = availablePlaces.shift();
+  
+  gameHistory.push(player + place);
+  
+  player = (player === 'X') ? 'O' : 'X';
+  
+  const winner = core.checkResult(gameHistory);
+  if (!winner) {
+    await doRandomGame(player, gameHistory, availablePlaces);
+  } else {
+    await setWinner(winner, gameHistory);
+  }
+}
+
+async function doCheckedGame (player, gameHistory, availablePlaces) {
+  const result = await checkBestChoice(player, gameHistory.join(';'));
+  let place;
+  if (result.length === 0) {
     availablePlaces = availablePlaces.sort(() => Math.random() - 0.5)
-    const place = availablePlaces.shift();
-
-    gameHistory.push(player + place);
-
-    player = (player === 'X') ? 'O' : 'X';
-
-    const winner = core.checkResult(gameHistory);
-    if (!winner) {
-        doRandomGame(player, gameHistory, availablePlaces);
-    } else {
-        setWinner(winner, gameHistory);
+    place = availablePlaces.shift();
+  } else {
+    let moves = {};
+    for (let i in result) {
+      let r = result[i];
+      moves[r._id.substring(1)] = r.winRate;
     }
-}
-
-function doCheckedGame (player, gameHistory, availablePlaces) {
-    let bestChoinPromise = checkBestChoice(player, gameHistory.join(';'));
-    bestChoinPromise.then((result) => {
-        let place;
-        if (result.length === 0) {
-            availablePlaces = availablePlaces.sort(() => Math.random() - 0.5)
-            place = availablePlaces.shift();
-        } else {
-            let moves = {};
-            for (let i in result) {
-                let r = result[i];
-                moves[r._id.substring(1)] = r.winRate;
-            }
     
-            availablePlaces.forEach(function(move) {
-                moves[move] = (moves[move] ? moves[move] : 0);
-            });
-            
-            // https://stackoverflow.com/a/1069840/6101515
-            const sortable = Object.fromEntries(
-                Object.entries(moves).sort(([,a],[,b]) => b-a)
-            );
-    
-            place = Object.keys(sortable)[0];
-            availablePlaces.splice(availablePlaces.indexOf(place), 1);
-        }
-
-        gameHistory.push(player + place);
-
-        player = (player === 'X') ? 'O' : 'X';
-
-        const winner = core.checkResult(gameHistory);
-        if (!winner) {
-            doCheckedGame(player, gameHistory, availablePlaces);
-        } else {
-            setWinner(winner, gameHistory);
-        }
+    availablePlaces.forEach(function(move) {
+      moves[move] = (moves[move] ? moves[move] : 0);
     });
+    
+    // https://stackoverflow.com/a/1069840/6101515
+    const sortable = Object.fromEntries(
+      Object.entries(moves).sort(([,a],[,b]) => b-a)
+    );
+      
+    place = Object.keys(sortable)[0];
+    availablePlaces.splice(availablePlaces.indexOf(place), 1);
+  }
+    
+  gameHistory.push(player + place);
+  
+  player = (player === 'X') ? 'O' : 'X';
+  
+  const winner = core.checkResult(gameHistory);
+  if (!winner) {
+    await doCheckedGame(player, gameHistory, availablePlaces);
+  } else {
+    await setWinner(winner, gameHistory);
+  }
+}
+  
+console.log('Learning...');
+async function startLearn (i) {
+  console.log('Playing match:', i, `of ${i}`);
+  let player = i%2 === 0 ? 'X' : 'O';
+  let gameHistory = [];
+  let availablePlaces = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'];
+  
+  if (randomLearn) {
+    await doRandomGame(player, gameHistory, availablePlaces);
+  } else {
+    await doCheckedGame(player, gameHistory, availablePlaces);
+  }
+  
+  if (i < times) {
+    startLearn(++i)
+  } else {
+    console.log('FINISHED.');
+    process.exit()
+  }
 }
 
-for (let i = 0; i < times; i++) { // Loop de jogos
-    let player = i%2 === 0 ? 'X' : 'O';
-    let gameHistory = [];
-    let availablePlaces = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'];
-
-    if (randomLearn) {
-        doRandomGame(player, gameHistory, availablePlaces);
-    } else {
-        doCheckedGame(player, gameHistory, availablePlaces);
-    }
-}
-
-console.log('Finishing...');
+startLearn(0)
